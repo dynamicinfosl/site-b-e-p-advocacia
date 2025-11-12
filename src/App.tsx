256f6fc7dea9d1d49480fe3ad1ed517c45b7bf85
Mileny Lois Vilas <mileny_loisvilas@outlook.com>
2025-11-12 12:13:42 -0300

    Ajusta redirecionamento da página e adiciona favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import faviconUrl from "./assets/images/logo/favicon.png";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+}
+
+function Favicon() {
+  useEffect(() => {
+    const existingLink = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
+
+    if (existingLink) {
+      existingLink.type = "image/png";
+      existingLink.href = faviconUrl;
+    } else {
+      const newLink = document.createElement("link");
+      newLink.rel = "icon";
+      newLink.type = "image/png";
+      newLink.href = faviconUrl;
+      document.head.appendChild(newLink);
+    }
+  }, []);
+
+  return null;
+}
 
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={__BASE_PATH__}>
+        <Favicon />
+        <ScrollToTop />
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
